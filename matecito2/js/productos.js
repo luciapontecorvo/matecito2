cc48be1b3a856d24c165d47ddbc6508ace6baf14
luciapontecorvo <136995124+luciapontecorvo@users.noreply.github.com>
2023-09-20 22:54:24 -0300

    cambio?

---
+++ matecito2/js/productos.js
@@ -0,0 +1,114 @@
+const productos = [
+{
+    id: 1,
+    nombre: "Mate Camionero Negro",
+    precio: 3500,
+    img:
+    "#",
+    cantidad: 1,
+},
+{
+    id: 2,
+    nombre: "Mate Alpaca",
+    precio: 3500,
+    img:
+    "#",
+    cantidad: 1,
+},
+    {
+    id: 3,
+    nombre: "Mate Escudo Argentina",
+    precio: 3500,
+    img:
+    "../multimedia/mates/matecamioneropremium.jpeg",
+    cantidad: 1,
+    },
+    {
+    id: 4,
+    nombre: "Termo Personalizado",
+    precio: 10000,
+    img:
+    "../multimedia/termos/matecamioneropremium.jpeg",
+    cantidad: 1,
+    },
+    {
+    id: 5,
+    nombre: "Termo Stanley Verde",
+    precio: 10000,
+    img:
+    "../multimedia/termos/matecamioneropremium.jpeg",
+    cantidad: 1,
+    },
+    {
+    id: 6,
+    nombre: "Termo Stanley Gold",
+    precio: 10000,
+    img:
+    "../multimedia/termos/matecamioneropremium.jpeg",
+    cantidad: 1,
+    },
+    {
+    id: 7,
+    nombre: "Bombilla Acero Inoxidable",
+    precio: 1500,
+    img:
+    "../multimedia/bombillas/matecamioneropremium.jpeg",
+    cantidad: 1,
+    },
+    {
+    id: 8,
+    nombre: "Bombilla Alpaca",
+    precio: 1500,
+    img:
+    "../multimedia/bombillas/matecamioneropremium.jpeg",
+    cantidad: 1,
+    },
+    {
+    id: 9,
+    nombre: "Bombillon",
+    precio: 1500,
+    img:
+    "../multimedia/bombillas/matecamioneropremium.jpeg",
+    cantidad: 1,
+    },
+    {
+    id: 10,
+    nombre: "Matera Marron",
+    precio: 5000,
+    img:
+    "../multimedia/materas/matecamioneropremium.jpeg",
+    cantidad: 1,
+    },
+    {
+    id: 11,
+    nombre: "Matera Negra",
+    precio: 5000,
+    img:
+    "../multimedia/materas/matecamioneropremium.jpeg",
+    cantidad: 1,
+    },
+    {
+    id: 12,
+    nombre: "Yerba Canarias",
+    precio: 1000,
+    img:
+    "../multimedia/yerbas/matecamioneropremium.jpeg",
+    cantidad: 1,
+    },
+    {
+    id: 13,
+    nombre: "Yerba Sara tradicional",
+    precio: 1000,
+    img:
+    "../multimedia/yerbas/matecamioneropremium.jpeg",
+    cantidad: 1,
+    },
+    {
+    id: 14,
+    nombre: "Yerba Baldo",
+    precio: 1000,
+    img:
+    "../multimedia/yerbas/matecamioneropremium.jpeg",
+    cantidad: 1,
+    },
+];
